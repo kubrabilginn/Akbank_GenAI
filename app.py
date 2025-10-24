27823a54ede1623013bc73ef9c1719f6f79d654b
kubrabilginn <htckbrblgn412@hotmail.com>
2025-10-24 13:44:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def cosine_similarity(a: np.ndarray, b: np.ndarray) -> float:
 # UI
 # ------------------------------------------------
 st.set_page_config(page_title="Yemek Tarifleri Chatbotu", layout="wide")
-st.title("🍽️ Akbank GenAI Yemek Tarifleri Chatbotu (HF Embedding + Groq LLM)")
+st.title("🍽️ GenAI Yemek Tarifleri Chatbotu")
 st.divider()
 
 # Embedding modelini yükle
