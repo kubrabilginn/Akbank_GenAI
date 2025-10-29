e13413c10a68c82a6ab575df535d02d1400bbbbd
Hatice Kübra Bilgin <htckbrblgn412@hotmail.com>
2025-10-29 12:34:29 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # run.sh: Uygulamayı başlatan ana script
 
-# 1. API ve LangSmith ayarlarını dış ortam değişkenlerinden al
-export GEMINI_API_KEY=AIzaSyDHB2gGL791C56W9dZFU9QIdMMBPUyRy58
-export LANGCHAIN_TRACING_V2="false"
-
 # 2. CACHE Dizinlerini Kullanıcının İzinli Alanına Yönlendirme
 # Bu, 'Permission denied' hatasını çözer.
 export XDG_CACHE_HOME=/home/appuser/.cache
